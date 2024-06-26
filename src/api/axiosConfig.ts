1f4ccfbb75551ef0596138df5652a6ec17ff4e91
Denis Wachira <deniswachira77@gmail.com>
2024-06-26 18:16:09 +0300

    url

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000", // Replace with your API base URL
+  baseURL: "https://book-repo-api.azurewebsites.net", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
   },
